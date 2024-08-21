4fabc83133359c0394a5e1942688b9bde3ba4dca
Quasi Developer <info@quasi.ai>
2024-08-21 12:30:17 -0400

    Fixed bug in condition checking

--- nav2_bringup/launch/navigation_launch.py
+++ nav2_bringup/launch/navigation_launch.py
@@ -17,7 +17,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction, SetEnvironmentVariable
-from launch.conditions import IfCondition
+from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import LoadComposableNodes, Node, SetParameter
 from launch_ros.descriptions import ComposableNode, ParameterFile
@@ -122,7 +122,7 @@ def generate_launch_description() -> LaunchDescription:
     )
 
     load_nodes = GroupAction(
-        condition=IfCondition(PythonExpression(['not ', use_composition])),
+        condition=UnlessCondition(use_composition),
         actions=[
             SetParameter('use_sim_time', use_sim_time),
             Node(
--- nav2_collision_monitor/launch/collision_monitor_node.launch.py
+++ nav2_collision_monitor/launch/collision_monitor_node.launch.py
@@ -19,7 +19,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction
-from launch.conditions import IfCondition
+from launch.conditions import IfCondition, UnlessCondition
 from launch.substitutions import LaunchConfiguration, NotEqualsSubstitution, PythonExpression
 from launch_ros.actions import LoadComposableNodes, Node, PushROSNamespace, SetParameter
 from launch_ros.descriptions import ComposableNode, ParameterFile
@@ -87,7 +87,7 @@ def generate_launch_description() -> LaunchDescription:
 
     # Declare node launching commands
     load_nodes = GroupAction(
-        condition=IfCondition(PythonExpression(['not ', use_composition])),
+        condition=UnlessCondition(use_composition),
         actions=[
             SetParameter('use_sim_time', use_sim_time),
             PushROSNamespace(
