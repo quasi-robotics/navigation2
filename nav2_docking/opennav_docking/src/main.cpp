e0623e105dee27947f473a9d337de0bf45af78bf
Quasi Developer <info@quasi.ai>
2026-09-04 13:20:53 -0400

    Use multi-threaded executor for docking server

--- nav2_docking/opennav_docking/src/main.cpp
+++ nav2_docking/opennav_docking/src/main.cpp
@@ -20,8 +20,10 @@
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
+  rclcpp::executors::MultiThreadedExecutor executor;
   auto node = std::make_shared<opennav_docking::DockingServer>();
-  rclcpp::spin(node->get_node_base_interface());
+  executor.add_node(node->get_node_base_interface());
+  executor.spin();
   rclcpp::shutdown();
 
   return 0;
